6e0ab293b24352a0c7e50aec2f5b3db21ea1890b
prashanthkarunakaran <147994003+prashanthkarunakaran@users.noreply.github.com>
2026-05-09 19:23:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ ingredients_list = st.multiselect(
     fruit_list,
     max_selections=5
 )
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
 if ingredients_list:
 
